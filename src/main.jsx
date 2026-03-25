aa276c4d7965f46f43dabec7377ce3369cb4cebe
Dinesh Bishnoi <93854544+svyamkrit@users.noreply.github.com>
2026-03-25 19:15:29 +0530

    push1

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/Pixelpenmediaa"> {/* 👈 add this */}
+    <BrowserRouter basename="/"> {/* 👈 add this */}
       <App />
     </BrowserRouter>
   </StrictMode>,
