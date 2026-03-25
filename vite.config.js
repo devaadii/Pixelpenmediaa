0dd9ad921c0b37f1384e221b8ae6837ee310334a
Dinesh Bishnoi <93854544+svyamkrit@users.noreply.github.com>
2026-03-25 19:10:55 +0530

    config push

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from "path"
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/Pixelpenmediaa/", // 👈 IMPORTANT (your repo name)
+  base: "/", // 👈 IMPORTANT (your repo name)
 
   plugins: [react()],
 
